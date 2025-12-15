9d2bb85041dfdd6c0cada7ccceb2d354c9ea5275
Sami J. Mäkinen <sjm@iki.fi>
2025-12-15 21:46:49 +0200

    tweaks

--- src/bin/nullidentd.rs
+++ src/bin/nullidentd.rs
@@ -1,11 +1,11 @@
 // bin/nullidentd
 
-use std::net;
-
 use clap::Parser;
-use tokio::io::{self, AsyncWriteExt, BufWriter};
-use tokio::net::{TcpListener, TcpStream};
-use tokio::{task, time};
+use tokio::{
+    io::{self, AsyncWriteExt, BufWriter},
+    net::{TcpListener, TcpStream},
+    runtime, task, time,
+};
 use tokio_stream::StreamExt;
 use tokio_util::codec::{FramedRead, LinesCodec};
 
@@ -17,9 +17,7 @@ fn main() -> Result<(), io::Error> {
     let opts = OptsCommon::parse();
     opts.start_pgm(env!("CARGO_BIN_NAME"));
 
-    let runtime = tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()?;
+    let runtime = runtime::Builder::new_multi_thread().enable_all().build()?;
 
     runtime.block_on(async move {
         let mut tasks = task::JoinSet::new();
@@ -28,11 +26,12 @@ fn main() -> Result<(), io::Error> {
         }
 
         while let Some(res) = tasks.join_next().await {
-            debug!("Server task result: {:?}", res);
+            debug!("Server task exited, result: {:?}", res);
         }
     });
     runtime.shutdown_timeout(time::Duration::new(5, 0));
     info!("Exit.");
+
     Ok(())
 }
 
@@ -44,28 +43,21 @@ async fn run_server(listen_addr: String, timeout: u64, ident: String) -> Result<
     loop {
         let (socket, c_addr) = listener.accept().await?;
         i += 1;
-        let cn = i;
 
         let id = ident.clone();
         let listen = listen_addr.clone();
+        let conn_n = i;
+
         tokio::spawn(async move {
-            if let Err(e) =
-                time::timeout(timeout, process_conn(id, &listen, socket, c_addr, cn)).await
-            {
-                error!("Connection ({listen})#{cn} timed out : {e}");
+            info!("New connection ({listen})#{conn_n} from {c_addr:?}");
+            if let Err(e) = time::timeout(timeout, process_conn(id, socket, conn_n)).await {
+                error!("Connection ({listen})#{conn_n} timed out : {e}");
             }
         });
     }
 }
 
-async fn process_conn(
-    ident: String,
-    listen_addr: &str,
-    mut socket: TcpStream,
-    addr: net::SocketAddr,
-    cn: u64,
-) -> Result<(), io::Error> {
-    info!("New connection ({listen_addr})#{cn} from {addr:?}");
+async fn process_conn(ident: String, mut socket: TcpStream, conn_n: u64) -> Result<(), io::Error> {
     let (sock_r, sock_w) = socket.split();
     let mut reader = FramedRead::new(sock_r, LinesCodec::new_with_max_length(BUF_SZ));
     let mut writer = BufWriter::new(sock_w);
@@ -75,7 +67,7 @@ async fn process_conn(
         writer.write_all(response.as_bytes()).await?;
         writer.flush().await?;
     }
-    info!("Connection ({listen_addr})#{cn} closed.");
+    info!("Connection #{conn_n} closed.");
     Ok(())
 }
 // EOF
